3b40917eca2c48adb0dbadbd6b0d36323e7a3a69
Mohamed Said <mohamed.said.fci@gmail.com>
2024-07-02 20:56:18 +0200

    add clippy job

--- src/services/db.rs
+++ src/services/db.rs
@@ -20,8 +20,8 @@ impl Database {
                 .unwrap();
 
         let client = Client::with_options(options).unwrap();
-        let db = client.database("blog-database");
+        
 
-        db
+        client.database("blog-database")
     }
 }
